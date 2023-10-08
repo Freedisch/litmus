b6215a3c58eb41c0b05e7f808e10f320e23f26fc
freedisch <freeproduc@gmail.com>
2023-10-08 22:23:58 +0100

    grpc tests
    Signed-off-by: freedisch <freeproduc@gmail.com>

--- chaoscenter/authentication/api/handlers/grpc/grpc_handler_test.go
+++ chaoscenter/authentication/api/handlers/grpc/grpc_handler_test.go
@@ -4,121 +4,97 @@ import (
 	"context"
 	"testing"
 
-	"github.com/google/uuid"
+	"github.com/golang-jwt/jwt"
+	"github.com/litmuschaos/litmus/chaoscenter/authentication/api/handlers/grpc"
 	"github.com/litmuschaos/litmus/chaoscenter/authentication/api/mocks"
 	"github.com/litmuschaos/litmus/chaoscenter/authentication/api/presenter/protos"
+	"github.com/litmuschaos/litmus/chaoscenter/authentication/pkg/entities"
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/mock"
 )
 
-var (
-	testGrpc = new(mocks.ServerGrpc)
-)
-
-func TestValidateRequest(t *testing.T) {
-	ctx := context.Background()
-	input := &protos.ValidationRequest{
-		Jwt:           uuid.NewString(),
-		ProjectId:     uuid.NewString(),
-		RequiredRoles: []string{uuid.NewString()},
-		Invitation:    uuid.NewString(),
-	}
-	testcases := []struct {
-		name    string
-		given   func()
-		isError bool
-	}{
-		{
-			name: "success",
-			given: func() {
-				testGrpc.On("ValidateRequest", ctx, input).Return(&protos.ValidationResponse{}, nil)
-			},
-			isError: false,
-		},
-	}
-	for _, tc := range testcases {
-		t.Run(tc.name, func(t *testing.T) {
-			// given
-			tc.given()
-			// when
-			response, err := testGrpc.ValidateRequest(ctx, input)
-			// then
-			if err != nil {
-				assert.Error(t, err)
-			} else {
-				assert.NoError(t, err)
-				assert.NotNil(t, response)
-			}
-		})
-	}
-
+type MockServer struct {
+	grpc.ServerGrpc
 }
 
-func TestGetProjectById(t *testing.T) {
-	ctx := context.Background()
-	input := &protos.GetProjectByIdRequest{
-		ProjectID: uuid.NewString(),
+func (m *MockServer) ValidateToken(token string) (*jwt.Token, error) {
+	claims := jwt.MapClaims{
+		"uid": "12345",
 	}
-	testcases := []struct {
-		name    string
-		given   func()
-		isError bool
-	}{
-		{
-			name: "success",
-			given: func() {
-				testGrpc.On("GetProjectById", ctx, input).Return(&protos.GetProjectByIdResponse{}, nil)
-			},
-			isError: false,
-		},
-	}
-	for _, tc := range testcases {
-		t.Run(tc.name, func(t *testing.T) {
-			// given
-			tc.given()
-			//when
-			response, err := testGrpc.GetProjectById(ctx, input)
-			// then
-			if err != nil {
-				assert.Error(t, err)
-			} else {
-				assert.NoError(t, err)
-				assert.NotNil(t, response)
-			}
-		})
-	}
+	return &jwt.Token{
+		Claims: claims,
+	}, nil
 }
 
-func TestGetUserById(t *testing.T) {
+// Mocking RbacValidator function
+func (m *MockServer) RbacValidator(uid, projectId string, requiredRoles []string, invitation bool, service interface{}) error {
+	return nil
+}
+
+func TestValidateRequest_Success(t *testing.T) {
+	s := &MockServer{}
+	req := &protos.ValidationRequest{
+		Jwt:           "valid-jwt",
+		ProjectId:     "project-id",
+		RequiredRoles: []string{"admin"},
+		Invitation:    "true",
+	}
+
 	ctx := context.Background()
-	input := &protos.GetUserByIdRequest{
-		UserID: uuid.NewString(),
+
+	// Call the function
+	resp, err := s.ValidateRequest(ctx, req)
+
+	// Assertions
+	assert.Nil(t, err, "Expected no error")
+	assert.NotNil(t, resp, "Expected non-nil response")
+	assert.True(t, resp.IsValid, "Expected the request to be valid")
+	assert.Empty(t, resp.Error, "Expected error message to be empty")
+}
+
+
+// Mocking ApplicationService methods
+type MockApplicationService struct {
+	mock.Mock
+}
+
+func TestGetProjectById_Success(t *testing.T) {
+	s := &grpc.ServerGrpc{
+		ApplicationService: &mocks.MockedApplicationService{},
 	}
-	testcases := []struct {
-		name    string
-		given   func()
-		isError bool
-	}{
-		{
-			name: "success",
-			given: func() {
-				testGrpc.On("GetUserById", ctx, input).Return(&protos.GetUserByIdResponse{}, nil)
+
+	// Mocking ApplicationService methods
+	mockService := s.ApplicationService.(*mocks.MockedApplicationService)
+	mockService.On("GetProjectByProjectID", "project-id").Return(&entities.Project{
+		ID:      "project-id",
+		Name:    "test-project",
+		Members: []*entities.Member{
+			{
+				UserID:    "user-1",
+				Invitation: entities.PendingInvitation,  // adjust to your actual type
+				JoinedAt:  1234567890,
 			},
-			isError: false,
 		},
+	}, nil)
+	mockService.On("FindUsersByUID", []string{"user-1"}).Return(&[]entities.User{
+		{
+			ID:       "user-1",
+			Email:    "user1@email.com",
+			Username: "user1",
+		},
+	}, nil)
+
+	req := &protos.GetProjectByIdRequest{
+		ProjectID: "project-id",
 	}
-	for _, tc := range testcases {
-		t.Run(tc.name, func(t *testing.T) {
-			// given
-			tc.given()
-			// when
-			response, err := testGrpc.GetUserById(ctx, input)
-			// then
-			if tc.isError {
-				assert.Error(t, err)
-			} else {
-				assert.NoError(t, err)
-				assert.NotNil(t, response)
-			}
-		})
-	}
+
+	ctx := context.Background()
+
+	resp, err := s.GetProjectById(ctx, req)
+
+	// Assertions
+	assert.Nil(t, err)
+	assert.NotNil(t, resp)
+	assert.Equal(t, "test-project", resp.Name)
+	assert.Equal(t, "user1@email.com", resp.Members[0].Email)
 }
